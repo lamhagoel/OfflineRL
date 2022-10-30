7bebc1115c6a4463f22dded9a60b8327ad809d7f
lamhagoel <94916432+lamhagoel@users.noreply.github.com>
2022-10-30 18:28:39 -0500

    Update common.py

--- offlinerl/utils/net/common.py
+++ offlinerl/utils/net/common.py
@@ -110,7 +110,8 @@ class Net(nn.Module):
         """Mapping: s -> flatten -> logits."""
 
         s = s.reshape(s.size(0), -1)
-        logits = self.model(torch.t(s))
+        print(s.size)
+        logits = self.model(s)
         if self.dueling is not None:  # Dueling DQN
             q, v = self.Q(logits), self.V(logits)
             logits = q - q.mean(dim=1, keepdim=True) + v
