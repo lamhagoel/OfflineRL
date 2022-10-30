88a173eec68c7e89a85e938a8779a40a7d672d90
lamhagoel <94916432+lamhagoel@users.noreply.github.com>
2022-10-30 18:46:04 -0500

    Update common.py

--- offlinerl/utils/net/common.py
+++ offlinerl/utils/net/common.py
@@ -110,7 +110,7 @@ class Net(nn.Module):
         """Mapping: s -> flatten -> logits."""
 
         s = s.reshape(s.size(0), -1)
-        print(s.size)
+        print(s.size())
         logits = self.model(s)
         if self.dueling is not None:  # Dueling DQN
             q, v = self.Q(logits), self.V(logits)
