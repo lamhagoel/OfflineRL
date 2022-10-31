2559e7c63c0cc4a25a23e30895d1f8319f094815
lamhagoel <94916432+lamhagoel@users.noreply.github.com>
2022-10-30 19:25:05 -0500

    Update common.py

--- offlinerl/utils/net/common.py
+++ offlinerl/utils/net/common.py
@@ -30,6 +30,8 @@ class BasePolicy(ABC):
     
     def get_action(self, obs):
         obs_tensor = torch.as_tensor(obs, device=next(self.parameters()).device, dtype=torch.float32)
+        if (obs_tensor.dim() == 1):
+            obs_tensor = obs_tesnor[None, :]
         act = to_array_as(self.policy_infer(obs_tensor), obs)
         
         return act
