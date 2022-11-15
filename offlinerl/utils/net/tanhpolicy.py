95e2e4acc05aa4fdb3ef2d6ae94072e9b59e15b0
lamhagoel <94916432+lamhagoel@users.noreply.github.com>
2022-11-14 19:40:39 -0600

    Update tanhpolicy.py

--- offlinerl/utils/net/tanhpolicy.py
+++ offlinerl/utils/net/tanhpolicy.py
@@ -147,7 +147,7 @@ class TanhGaussianPolicy(ActorProb, BasePolicy):
         :param deterministic: If True, do not sample
         :param return_log_prob: If True, return a sample and its log probability
         """
-        print("Obs size: " + str(obs.size()))
+#         print("Obs size: " + str(obs.size()))
         logits, h = self.preprocess(obs, state)
         mean = self.mu(logits)
         
